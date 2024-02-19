ad24fa7ee372bfa70ee52e8a11a1e6cf80c6bae3
pforprayag <pforprayag@gmail.com>
2024-02-18 13:32:48 -1200

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/about" element={<About/>} />
+        <Route exact path="/signin" element={<Signin/>} />
+        <Route exact path="/signup" element={<Signup/>} />
+        <Route exact path="/dashboard" element={<Dashboard/>} />
+        <Route exact path="/projects" element={<Projects/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
